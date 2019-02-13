21dc0fd197a936883bd9857cd42a5e7ad01a5743
blakematis <14blake1994@gmail.com>
2019-02-13 14:20:19 -0800

    Added most recent Midas API documentation.

---
+++ src/test/java/midas/TestRunner.java
@@ -0,0 +1,22 @@
+package midas;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+
+    public static void main(String[] args){
+        Result result = JUnitCore.runClasses();
+
+        int testsRan = result.getRunCount();
+        int failedTests = result.getFailureCount();
+
+        for(Failure failure: result.getFailures()){
+            System.out.println(failure.toString());
+        }
+
+        System.out.println("Successful tests" + (testsRan-failedTests) + " / " + testsRan);
+
+    }
+}
