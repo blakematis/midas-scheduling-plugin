70ee5b56b7abbf5d1b54a30fc146237378a5b89f
blakematis <14blake1994@gmail.com>
2019-02-14 18:23:04 -0800

    Added all API supported commands/actions.

--- src/main/java/midas/constants/Actions.java
+++ src/main/java/midas/constants/Actions.java
@@ -10,5 +10,43 @@ public class Actions {
     public static final String AUTHENTICATE_USER = "authenticate_user";
     public static final String DEL_BOOKING = "del_booking";
     public static final String DEL_BOOKINGS = "del_bookings";
+    public static final String DEL_CLIENT = "del_client";
+    public static final String DEL_INVOICE = "del_invoice";
+    public static final String DEL_RESOURCE = "del_resource";
+    public static final String EMAIL_CLIENT = "email_client";
+    public static final String GEN_INVOICE = "gen_invoice";
+    public static final String GET_ACTIVITY = "get_activity";
+    public static final String GET_AVAILABILITY = "get_availability";
+    public static final String GET_BOOKING = "get_booking";
+    public static final String GET_BOOKINGS = "get_bookings";
+    public static final String GET_CLIENT = "get_client";
+    public static final String GET_CONSUMABLE_LEVELS = "get_consumable_levels";
+    public static final String GET_INVOICE = "get_invoice";
+    public static final String GET_INVOICES = "get_invoices";
+    public static final String GET_MESSAGES = "get_messages";
+    public static final String GET_REMINDERS = "get_reminders";
+    public static final String GET_RESOURCE = "get_resource";
+    public static final String GET_RESOURCE_AVAILABILITY = "get_resource_availability";
+    public static final String GET_SETTING = "get_setting";
+    public static final String GET_TEMPLATE = "get_template";
+    public static final String GET_USER = "get_user";
+    public static final String GET_USERS_LOGGED_IN = "get_users_logged_in";
+    public static final String GET_VENUE = "get_venue";
+    public static final String GET_VENUES = "get_venues";
+    public static final String GET_VENUES_IN_GROUP = "get_venues_in_group";
+    public static final String GET_VENUES_IN_USE = "get_venues_in_use";
+    public static final String GET_WATCHES = "get_watches";
+    public static final String MOD_BOOKING = "mod_booking";
+    public static final String MOD_CLIENT = "mod_client";
+    public static final String MOD_INVOICE = "mod_invoice";
+    public static final String MOD_RESOURCE = "mod_resource";
+    public static final String NOTIFY_USER = "notify_user";
+    public static final String REJECT_REQUEST = "reject_request";
+    public static final String RESTORE_BOOKING = "restore_booking";
+    public static final String SET_SETTING = "set_setting";
+    public static final String SET_TEMPLATE = "set_template";
+    public static final String UTIL_FROM_EPOCH = "util_from_epoch";
+    public static final String UTIL_TO_EPOCH = "util_to_epoch";
 
+    
 }
