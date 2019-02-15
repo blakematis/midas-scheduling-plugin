2adeaf26ac1b2b1f95ed96af02e2a19b530cfa93
blakematis <14blake1994@gmail.com>
2019-02-15 01:47:27 -0800

    Removed Static import

--- src/test/java/midas/MidasRequestClientTest.java
+++ src/test/java/midas/MidasRequestClientTest.java
@@ -5,14 +5,12 @@ import midas.requests.GetBookingRequest;
 import javax.json.JsonObject;
 import java.net.MalformedURLException;
 
-import static midas.MidasRequestClient.getMidasResponse;
-
 public class MidasRequestClientTest {
     //TODO
 
     public static void main(String[] args) throws MalformedURLException {
         GetBookingRequest bookingRequest = new GetBookingRequest(0);
-        JsonObject bookingResponse = getMidasResponse(bookingRequest);
+        JsonObject bookingResponse = MidasRequestClient.getMidasResponse(bookingRequest);
         System.out.println(bookingResponse.toString());
     }
 }
